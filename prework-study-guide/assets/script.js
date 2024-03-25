6b773b2ae1eeba146ac3fa25c10dc23fe1944e04
Trevor <71895290+trevorapena@users.noreply.github.com>
2024-03-25 18:27:07 -0400

    Update function to handle edge cases

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -2,23 +2,29 @@ const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 const randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
 function listTopics() {
- for (let x = 0; x < topics.length; x++) {
-   console.log(topics[x]);
- }
+    for (let x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
 }
 
 function selectTopic() {
- if (randomTopic === 'HTML') {
-   console.log("Let's study HTML!");
- } else if (randomTopic === 'CSS') {
-   console.log("Let's study CSS!");
- } else if (randomTopic === 'Git') {
-   console.log("Let's study Git!");
- } else if (randomTopic === 'JavaScript') {
-   console.log("Let's study JavaScript!");
- } else {
-   console.log('Please try again!');
- }
+    switch (randomTopic) {
+        case 'HTML':
+            console.log("Let's study HTML!");
+            break;
+        case 'CSS':
+            console.log("Let's study CSS!");
+            break;
+        case 'Git':
+            console.log("Let's study Git!");
+            break;
+        case 'JavaScript':
+            console.log("Let's study JavaScript!");
+            break;
+        default:
+            console.log('Please try again!');
+            break;
+    }
 }
 
 console.log('Here are the topics we learned through Prework:');
